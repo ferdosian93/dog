57b055b87af1ea1104feb0c76294ed55b44e8225
m.ferdosian <m.ferdosian@asanpardakht.com>
2024-05-28 10:46:02 +0330

    add new method

--- dog.go
+++ dog.go
@@ -1,8 +1,13 @@
 package dog
+
 import (
 	"strings"
 )
 
 func WhenGrownUp(s string) string {
 	return "when the puppy grows up it says:" + strings.ToUpper(s)
+}
+
+func WhenGetHungry(s string) string {
+	return "when he get hungry says:" + strings.ToUpper(s)
 }
